d892c3c9b73a64e85ce934327d96c0cd2bd4379c
ext.wangxiaoran3 <ext.wangxiaoran3@jd.com>
2023-03-03 11:04:24 +0800

    1.2.0

---
+++ FrameWork-1.2.0/ios/XROCSupport.framework/Versions/A/Headers/XROCSupport.h
@@ -0,0 +1,25 @@
+//
+//  XROCSupport.h
+//  XROCSupport
+//
+//  Created by ext.wangxiaoran3 on 2022/11/24.
+//
+
+#ifndef XROCSupport_h
+#define XROCSupport_h
+
+//A
+#import <XROCSupport/XRPCH.h>
+
+//Base
+#import <XROCSupport/XRBaseViewController.h>
+#import <XROCSupport/XRBaseNavigationController.h>
+
+//Category
+#import <XROCSupport/NSObject+Property.h>
+#import <XROCSupport/NSString+STRegex.h>
+#import <XROCSupport/UIFont+XRScale.h>
+#import <XROCSupport/UIImage+ReColor.h>
+#import <XROCSupport/NSBundle+XRBundle.h>
+
+#endif /* XROCSupport_h */
